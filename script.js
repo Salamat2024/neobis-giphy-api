2bb66179687eacfc3f683821f6b73609f9dff0c2
Salamat <162944233+Salamat2024@users.noreply.github.com>
2024-05-16 11:17:45 +0600

    add function return giphys

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 
-fetch("https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
-.then(res=>res.json())
-.then(json=>console.log(json))
+// fetch("https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+
+
+// https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+
+async function getResponse() {
+    let response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=dogs&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+    let content = await response.json();
+  
+    const container = document.querySelector('.image-container');
+    container.innerHTML = '';
+  
+    content.data.forEach(gif => {
+      const img = document.createElement('img');
+      img.src = gif.images.fixed_height.url;
+      container.appendChild(img);
+    
+    });
+  }
+  
+  getResponse();
+  
+
+
+
+
+
+
+
+
+
+// for (key in content){
+
+    //     card.innerHTML += `<li class="post">
+    //                         <h4>${content[key.title]}</h4>
+    //                         img src="${content[key].url}"
+    //                       </li>`
+    //   console.log( content[key])
