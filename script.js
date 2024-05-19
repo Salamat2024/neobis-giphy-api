640fec512ad35fd3a136017093fcd1f1120a25f0
Salamat <162944233+Salamat2024@users.noreply.github.com>
2024-05-19 20:01:57 +0600

    remake API for getting an any giphy's

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
 
-// fetch("https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
-// .then(res=>res.json())
-// .then(data=>console.log(data))
+async function getResponse(query) {
+  const apiKey = 'PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH';
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
+  const content = await response.json();
 
+  const container = document.querySelector('.image-container');
+  container.innerHTML = '';
 
-// https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+  content.data.forEach(gif => {
+    const img = document.createElement('img');
+    img.src = gif.images.fixed_height.url;
+    container.appendChild(img);
+  });
+}
 
-async function getResponse() {
-    let response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=PV2ZWWf3tgqbbZ81FxfGo7ORrrCHVrRH&q=lion&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
-    let content = await response.json();
-  
-    const container = document.querySelector('.image-container');
-    const wrapper=document.querySelector('.wrapper')
-    container.innerHTML = '';
-    // wrapper.innerHTML='';
-  
-    content.data.forEach(gif => {
-      const img = document.createElement('img');
-      img.src = gif.images.fixed_height.url;
-      container.appendChild(img);
-
-    //   const images=document.createElement('img')
-    //   images.src= gif.images.fixed_height.url;
-    //   wrapper.appendChild(images)
-        
-    });
+document.getElementById('header-search').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const query = event.target.value;
+    getResponse(query);
   }
+});
+getResponse()
   
-  getResponse();
-  
 
 
 
 
 
 
-
-
-
-// for (key in content){
-
-    //     card.innerHTML += `<li class="post">
-    //                         <h4>${content[key.title]}</h4>
-    //                         img src="${content[key].url}"
-    //                       </li>`
-    //   console.log( content[key])
